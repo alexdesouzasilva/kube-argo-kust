432552a1780f898088f45a9de78edcbdfa506842
Alex <alexdesouzasilva94@gmail.com>
2024-09-11 19:44:56 -0300

    modificando resource

--- src/main/java/br/com/devsenior/kube_argo_kust/resources/KubeResource.java
+++ src/main/java/br/com/devsenior/kube_argo_kust/resources/KubeResource.java
@@ -1,5 +1,7 @@
 package br.com.devsenior.kube_argo_kust.resources;
 
+import java.time.Instant;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,7 @@ public class KubeResource {
 
     @GetMapping(value = "/health")
     public ResponseEntity<String> health() {
-        String health = "Status Ok";
+        String health = "Status Ok " + Instant.now();
         return ResponseEntity.ok(health);
     }
     
